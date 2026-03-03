f3c2cc84a34889d841b334a6bba6bbdfa383c175
wahabrej <awahabreja@gmail.com>
2026-03-03 11:57:13 +0600

    added the code

--- lib/features/home/presentaion/screen/home_screen.dart
+++ lib/features/home/presentaion/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:partsrunner/core/widget/customButton.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,10 +10,9 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            // Header image
             Container(
               width: double.infinity,
-              height: 250,
+              height: 280,
               decoration: BoxDecoration(
                 image: DecorationImage(
                   image: AssetImage('assets/images/header.png'),
@@ -20,6 +20,7 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
               child: Container(
+                padding: EdgeInsets.only(top: 50),
                 decoration: BoxDecoration(
                   gradient: LinearGradient(
                     begin: Alignment.topCenter,
@@ -30,69 +31,143 @@ class HomeScreen extends StatelessWidget {
                     ],
                   ),
                 ),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.white,
-                          ),
-
-                          child: Text(
-                            "AB",
-                            style: TextStyle(color: Colors.black),
-                          ),
-                        ),
-                        SizedBox(width: 7),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Good morning, James!",
-                              style: TextStyle(color: Color(0xffF86816)),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.white,
                             ),
-                            Text(
-                              "Find Your Runner?",
-                              style: TextStyle(color: Colors.white),
+
+                            child: Text(
+                              "AB",
+                              style: TextStyle(color: Colors.black),
                             ),
-                          ],
-                        ),
-                        Spacer(),
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(color: Colors.white),
                           ),
-                          child: Icon(
-                            Icons.notifications_none_outlined,
-                            color: Colors.white,
+                          SizedBox(width: 7),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Good morning, James!",
+                                style: TextStyle(color: Color(0xffF86816)),
+                              ),
+                              Text(
+                                "Find Your Runner?",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
                           ),
-                        ),
-                      ],
-                    ),
-
-                    TextFormField(
-                      decoration: InputDecoration(
-
+                          Spacer(),
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(color: Colors.white),
+                            ),
+                            child: Icon(
+                              Icons.notifications_none_outlined,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
                       ),
-                    )
-                  ],
+                      SizedBox(height: 20),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                            borderRadius: BorderRadius.circular(13),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                            borderRadius: BorderRadius.circular(13),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                            borderRadius: BorderRadius.circular(13),
+                          ),
+                          hintText: "Search....",
+                          filled: true,
+                          fillColor: Colors.white,
+
+                          suffixIcon: Padding(
+                            padding: EdgeInsets.all(3.0),
+                            child: Container(
+                              padding: EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Color(0xffFFECE6),
+                              ),
+                              child: Icon(Icons.search, color: Color(0xffFF4000)),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
+            Transform.translate(
+              offset: Offset(0, -90),
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        spreadRadius: 3,
+                        blurRadius: 4,
+                        offset: Offset(1, 1),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Color(0xffFFECE6),
+                            ),
+                            child: Icon(Icons.search, color: Color(0xffFF4000)),
+                          ),
 
-            // Content section
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
+                          SizedBox(width: 7),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Request New Delivery"),
+                              SizedBox(height: 5),
+                              Text("Schedule a parts pickup in seconds"),
+                            ],
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 20),
 
-                ],
+                      CustomButton(
+                        text: "Start Plan",
+                        submit: () {},
+                        backgroundColor: Color(0xffFF4000),
+                        textColor: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           ],
